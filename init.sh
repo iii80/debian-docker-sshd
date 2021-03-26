e3361b27566faeeec7db59964da8b9f92e05bbf0
tlze <whytlze@gmail.com>
2021-03-26 15:59:43 +0800

    Optimze init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,7 @@
 set -x
 
 apt update
-apt upgrade -y
-
-apt install -y openssh-server vim screen unzip curl
+apt install -y openssh-server vim screen unzip curl locales tzdata
 
 # 允许密码登录
 mkdir /var/run/sshd
@@ -13,11 +11,10 @@ sed -i "/PermitRootLogin/s/#Permit/Permit/" /etc/ssh/sshd_config
 sed -i "/PermitRootLogin/s/prohibit-password/yes/" /etc/ssh/sshd_config
 sed -i "s/UsePAM yes/#UsePAM yes/g" /etc/ssh/sshd_config
 
+# 设置root密码
 echo 'root:debian' | chpasswd
 
 # 安装配置系统语言环境
-apt install locales -y
-
 sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
 sed -i '/zh_CN.UTF-8/s/^# //g' /etc/locale.gen
 
@@ -25,7 +22,6 @@ locale-gen
 update-locale LANG=en_US.UTF-8
 
 # 配置系统时区
-apt install tzdata -y
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
