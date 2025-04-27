3a11c1adb83b069e534bdc4e3ecece3d4c3d846b
iii80 <myfriends123@gmail.com>
2025-04-27 22:18:15 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,8 +2,9 @@
 
 set -x
 
+#安装常用软件
 apt update
-apt install -y openssh-server vim screen unzip curl locales tzdata
+apt install -y openssh-server vim screen locales tzdata htop nload ncdu wget curl tar gzip bzip2 xz-utils unzip net-tools sudo ca-certificates
 
 # 允许密码登录
 mkdir /var/run/sshd
